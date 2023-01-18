7546e0b8266df62d4ab8d7670bb417f89d826378
Pedro Barros <pedrobars7a@gmail.com>
2023-01-18 10:34:04 -0300

    My Solution

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostBinding, HostListener, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective implements OnInit {
+  @HostBinding('class.open') isOpen: boolean = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
