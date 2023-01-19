c050918e4380671be2445770d60e83c04336d30f
Pedro Barros <pedrobars7a@gmail.com>
2023-01-19 14:54:20 -0300

    Using a Service for Cross-Component Communication

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,7 +11,11 @@ import { RecipeService } from './services/recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
+      this.selectedRecipe = recipe;
+    });
+  }
 }
--- src/app/recipes/services/recipe.service.ts
+++ src/app/recipes/services/recipe.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 export class RecipeService {
   constructor() {}
+
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -11,7 +13,7 @@ export class RecipeService {
       'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1280,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'
     ),
     new Recipe(
-      'A Test Recipe',
+      'Another Test Recipe',
       'This is a simply a test',
       'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1280,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'
     ),
